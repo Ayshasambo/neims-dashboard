044a8f4f13188622bfb6bf706e289e565793d020
Ayshasambo <aishamsambo94@gmail.com>
2023-09-28 16:43:31 +0100

    created item model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const { application } = require('express');
+const nodemon = require('nodemon');
 require('dotenv/config');
 
 //connect to database
---
+++ models/items.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    
+});
+
+
+module.exports = mongoose.model("Item", itemSchema);
