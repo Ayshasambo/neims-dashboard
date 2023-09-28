6b04d4ea50c23fd538b5e817e527ef1be9739c06
Ayshasambo <aishamsambo94@gmail.com>
2023-09-28 15:17:10 +0100

    created item item model

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { application } = require('express');
+require('dotenv/config');
+
+//connect to database
+mongoose.connect(process.env.DB_CONNECTION)
+.then(() => console.log('connected to mongodb...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
+
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
