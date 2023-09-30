698a3de35049651350a95054f4f0dd46dad6ebeb
Ayshasambo <aishamsambo94@gmail.com>
2023-09-30 09:46:21 +0100

    created station schema

---
+++ models/Station.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const stationSchema = new mongoose.Schema({
+    
+});
+
+
+module.exports = mongoose.model("Station", stationSchema);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 
 const userchema = new mongoose.Schema({
-    
+    name:{
+        type: String
+    },
+    surname:{
+        type: String
+    },
+    email:{
+        type: String,
+        unique: true
+    },
+    password:{
+        type: Sring
+    }
 });
 
 
