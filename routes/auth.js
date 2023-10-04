f30e073b1fa561ec66bf3e38bfb236933cdfad9a
Ayshasambo <aishamsambo94@gmail.com>
2023-10-04 13:56:51 +0100

    User authentication with jwt added

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,51 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
-const CryptoJS = require('crypto-js');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
-//REGISTER A USER
+// REGISTER A USER
 router.post("/register", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(400).send('User already registered');
 
-    const user = await User.findOne({userid: req.body.userid});
-    if (user) return res.status(400).send('User already registered');
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-   const newUser =  new User({
-       userid: req.body.userid,
-       password:  CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
-   });   
-   try{
-       const user = await newUser.save();
-       res.status(201).json('User successfully registered');
-      
-   }
-   catch(err){
-       res.status(500).json(err);
-   }
-  
- });
+        const newUser = new User({
+            userid: req.body.userid,
+            fullname: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: hashedPassword,
+        });
 
- module.exports = router;
+        const savedUser = await newUser.save();
 
+        // Generate JWT token
+        const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET, { expiresIn: '24h' }); 
+        res.status(201).json({ message: 'User successfully registered', token });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// LOG IN A USER
+ router.post("/login", async (req, res) => {
+    try {
+         const user = await User.findOne({ email: req.body.email });
+         if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+         const validPassword = await bcrypt.compare(req.body.password, user.password);
+         if (!validPassword) return res.status(401).json({ message: 'Invalid credentials' });
+
+         // Generate JWT token
+         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Adjust the expiration time as needed
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (err) {
+         res.status(500).json(err);
+        }
+});
+
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const CryptoJS = require('crypto-js')
-const jwt = require('jsonwebtoken')
-const User = require('../models/User.js')
+const bcrypt = require('bcrypt'); // Updated import
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
+
+
+//GET USER
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL USERS
+router.get("/",  async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
+
 
 //REGISTER A USER
-router.post("/", async (req, res) => {
+// router.post("/", async (req, res) => {
 
-    const user = await User.findOne({userid: req.body.userid});
-    if (user) return res.status(400).send('User already registered');
+//     const user = await User.findOne({userid: req.body.userid});
+//     if (user) return res.status(400).send('User already registered');
 
-   const newUser =  new User({
-       userid: req.body.userid,
-       name: req.body.name,
-       surname: req.body.surname,
-       email: req.body.email,
-       password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+//    const newUser =  new User({
+//        userid: req.body.userid,
+//        name: req.body.name,
+//        surname: req.body.surname,
+//        email: req.body.email,
+//        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
        
-   });   
-   try{
-       const user = await newUser.save();
-       res.status(201).json('User successfully registered');
+//    });   
+//    try{
+//        const user = await newUser.save();
+//        res.status(201).json('User successfully registered');
      
-   }
-   catch(err){
-       res.status(500).json(err);
-   }
+//    }
+//    catch(err){
+//        res.status(500).json(err);
+//    }
+// });
+
  
- });
+
 
  module.exports = router;
