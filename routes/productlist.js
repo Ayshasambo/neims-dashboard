de9c8b95fe35a857ca78393faa5bfa6c70915164
Ayshasambo <aishamsambo94@gmail.com>
2023-10-28 19:35:37 +0100

    create bin card entry

--- routes/productlist.js
+++ routes/productlist.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Productlist = require('../models/Productlist');
+const Bincard = require('../models/Bincard');
 const Station = require('../models/Station');
 const Category = require('../models/Category');
 const User = require('../models/User');
