b8d47e6063eb092327055c9b02823d94947150d3
Ayshasambo <aishamsambo94@gmail.com>
2023-10-29 12:58:02 +0100

    as

--- routes/productlist.js
+++ routes/productlist.js
@@ -79,6 +79,17 @@ router.put('/:id/outgoing', async (req, res) => {
     // Update quantity
     productlist.quantity -= quantity;
 
+    // Fetch category total from the database
+    const populatedCategory = await Category.findById(productlist.category.id);
+
+    if (!populatedCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    // Update category total
+    populatedCategory.total -= quantity;
+    await populatedCategory.save();
+
     // Create a new bincard entry
     const newBincardentry = new Bincard({ productlist: productlistId, quantity, reason: 'Outgoing' });
     await newBincardentry.save();
@@ -88,18 +99,6 @@ router.put('/:id/outgoing', async (req, res) => {
     }
     await productlist.save();
 
-    // Update category total
-   const populatedCategoryEntry = await Category.findById(productlist.category.id);
-   populatedCategoryEntry.total -= quantity;
-   await populatedCategoryEntry.save();
-
-   // Update station total
-   const populatedStation = await Station.findById(productlist.station.id);
-   populatedStation.total -= quantity;
-   await populatedStation.save();
-
-    
-    
 
     res.json(productlist);
   } catch (error) {
